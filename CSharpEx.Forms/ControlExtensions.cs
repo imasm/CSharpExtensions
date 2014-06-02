d6b9035cb0b2876132757b0c909a208bcae9c0d0
Ivan Masmitjà <imasmitja@gmail.com>
2014-06-02 18:21:37 +0200

    Add InvokeIfRequiered extensions

---
+++ CSharpEx.Forms/ControlExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharpEx.Forms
+{
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Invoke action if Invoke is requiered.
+        /// </summary>
+        public static void InvokeIfRequired<T>(this T c, Action<T> action) where T : Control
+        {
+            if (c.InvokeRequired)
+            {
+                c.Invoke(new Action(() => action(c)));
+            }
+            else
+            {
+                action(c);
+            }
+        }
+    }
+}
