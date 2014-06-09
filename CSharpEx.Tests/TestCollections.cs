848a29f652a20553cfadab30afe6c146009531c8
Ivan Masmitjà <imasmitja@gmail.com>
2014-06-09 11:33:55 +0200

    Add double comparisions and IsNullOrEmpty for collections

---
+++ CSharpEx.Tests/TestCollections.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CSharpEx.Tests
+{
+    [TestFixture]
+    public class TestCollections
+    {
+
+        [Test]
+        public void TestIsNullOrEmptyList()
+        {
+            List<int> nullList = null;
+            List<int> emptyList = new List<int>();
+            List<int> list = new List<int> {1, 2, 3};
+            
+            Assert.IsTrue(nullList.IsNullOrEmpty());
+            Assert.IsTrue(emptyList.IsNullOrEmpty());
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+    }
+}
