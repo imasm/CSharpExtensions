dc885fdaa751777ec9fc3b03f224c78c63979020
Ivan Masmitjà <imasmitja@gmail.com>
2014-06-25 10:55:11 +0200

    Add decimal extensions

---
+++ CSharpEx/DecimalEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpEx
+{
+    /// <summary>
+    /// Decimal extensions
+    /// </summary>
+    public static class DecimalEx
+    {
+        /// <summary>
+        /// Returns a number rounded to the desired multiple
+        /// </summary>
+        public static decimal MRound(this decimal value, decimal divisor)
+        {
+            return Math.Truncate(value / divisor) * divisor;
+        }
+            
+    }
+}
