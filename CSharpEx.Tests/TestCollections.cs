571f5a78a86c93bba29a111dac12e2975e7f7bca
Ivan Masmitjà <imasmitja@gmail.com>
2014-06-25 10:56:31 +0200

    Add find in collection

--- CSharpEx.Tests/TestCollections.cs
+++ CSharpEx.Tests/TestCollections.cs
@@ -6,17 +6,27 @@ namespace CSharpEx.Tests
     [TestFixture]
     public class TestCollections
     {
+        readonly List<int> _nullList = null;
+        readonly List<int> _emptyList = new List<int>();
+        readonly List<int> _list = new List<int> { 1, 2, 3 };
 
         [Test]
         public void TestIsNullOrEmptyList()
         {
-            List<int> nullList = null;
-            List<int> emptyList = new List<int>();
-            List<int> list = new List<int> {1, 2, 3};
-            
-            Assert.IsTrue(nullList.IsNullOrEmpty());
-            Assert.IsTrue(emptyList.IsNullOrEmpty());
-            Assert.IsFalse(list.IsNullOrEmpty());
+            Assert.IsTrue(_nullList.IsNullOrEmpty());
+            Assert.IsTrue(_emptyList.IsNullOrEmpty());
+            Assert.IsFalse(_list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void TestFirstIndex()
+        {
+            Assert.AreEqual(-1, _nullList.FirstIndex(x => x == 3));
+            Assert.AreEqual(-1, _emptyList.FirstIndex(x => x == 3));
+            Assert.AreEqual(0, _list.FirstIndex(x => x == 1));
+            Assert.AreEqual(1, _list.FirstIndex(x => x == 2));
+            Assert.AreEqual(2, _list.FirstIndex(x => x == 3));
+            Assert.AreEqual(-1, _list.FirstIndex(x => x == 4));
         }
     }
 }
--- CSharpEx/CollectionsEx.cs
+++ CSharpEx/CollectionsEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpEx
@@ -25,6 +26,26 @@ namespace CSharpEx
                 return collection.Count < 1;
             }
             return !enumerable.Any();
+        }
+
+        ///<summary>Finds the index of the first item matching an expression in an enumerable.</summary>
+        ///<param name="items">The enumerable to search.</param>
+        ///<param name="predicate">The expression to test the items against.</param>
+        ///<returns>The index of the first matching item, or -1 if no items match.</returns>
+        public static int FirstIndex<T>(this IEnumerable<T> items, Predicate<T> predicate)
+        {
+            if (items == null)
+                return -1;
+
+            int index = 0;
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                    return index;
+
+                index++;
+            }
+            return -1;
         }
     }
 }
