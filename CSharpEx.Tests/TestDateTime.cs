53852ed3f95d3bca3b0ee48802d6e797169f2f48
Ivan Masmitjà <imasmitja@gmail.com>
2014-06-25 11:08:01 +0200

    Add DateTime tests

---
+++ CSharpEx.Tests/TestDateTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharpEx.Tests
+{
+    [TestFixture]
+    public class TestDateTime
+    {
+        [Test]
+        public void TestDayOfWeekEU()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.AreEqual(i, new DateTime(2014, 1, 6 + i).DayOfWeekEU(), "Day " + i);
+            }
+        }
+    }
+}
