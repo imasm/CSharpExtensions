cc7b429d1865ddd264fed035e5469779849c10a8
Ivan Masmitjà <imasmitja@gmail.com>
2014-08-19 18:02:29 +0200

    NuGet package 0.0.8

--- CSharpEx/Properties/AssemblyVersion.cs
+++ CSharpEx/Properties/AssemblyVersion.cs
@@ -29,5 +29,5 @@ using System.Reflection;
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.7.0")]
-[assembly: AssemblyFileVersion("0.0.7.0")]
+[assembly: AssemblyVersion("0.0.8.0")]
+[assembly: AssemblyFileVersion("0.0.8.0")]
